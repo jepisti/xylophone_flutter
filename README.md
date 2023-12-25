2c897636b9120ac9a829b075cae32576aa2d0951
jaromi.istvan <jaromi.istvan@gmail.com>
2023-12-25 14:12:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# xylophone_flutter
+
+Xylophone music project
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
---
+++ android/app/src/main/kotlin/hu/jaromi/xylophone_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package hu.jaromi.xylophone_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
