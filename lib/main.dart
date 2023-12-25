ea2a32cfe3b8766476a41a7171ca4830d950b08b
jaromi.istvan <jaromi.istvan@gmail.com>
2023-12-25 16:35:42 +0100

    shorter code

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,35 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Future<void> playSound(int soundNumber) async {
+    AudioCache.instance = AudioCache(prefix: '');
+    final player = AudioPlayer();
+    await player.setVolume(1.0);
+    await player.play(AssetSource('assets/note$soundNumber.wav'));
+  }
+
+  Expanded buildKey(
+      {required int soundNum,
+      required Color foreColor,
+      required Color backColor}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () async {
+          playSound(soundNum);
+        },
+        style: TextButton.styleFrom(
+          foregroundColor: foreColor,
+          backgroundColor: backColor,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 50.0,
+            vertical: 5.0,
+          ),
+        ),
+        child: const Text('Click me'),
+      ),
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -19,17 +48,40 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
-            child: TextButton(
-              onPressed: () async {
-                AudioCache.instance = AudioCache(prefix: '');
-                final player = AudioPlayer();
-                await player.setVolume(1.0);
-                //await player.play(AssetSource('/note1.wav'));
-                await player.play(AssetSource('assets/note1.wav'));
-              },
-              child: const Text('Click me'),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildKey(
+                    soundNum: 1,
+                    foreColor: Colors.teal,
+                    backColor: Colors.amber),
+                buildKey(
+                    soundNum: 2,
+                    foreColor: Colors.white,
+                    backColor: Colors.blueGrey),
+                buildKey(
+                    soundNum: 3,
+                    foreColor: Colors.black26,
+                    backColor: Colors.blue),
+                buildKey(
+                    soundNum: 4,
+                    foreColor: Colors.black,
+                    backColor: Colors.amberAccent),
+                buildKey(
+                    soundNum: 5, foreColor: Colors.red, backColor: Colors.blue),
+                buildKey(
+                    soundNum: 6,
+                    foreColor: Colors.white,
+                    backColor: Colors.indigo),
+                buildKey(
+                    soundNum: 7,
+                    foreColor: Colors.teal,
+                    backColor: Colors.yellowAccent),
+              ],
             ),
           ),
         ),
